52faf5aa39bd62973e2faeac321221bbeefac6dc
bryanguerineau <bryan.guerineau@outlook.com>
2023-01-18 17:36:11 +0100

    Creation de addrinfo Ajout des packages

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include "functions.h"
 
 int main(int argc, char * argv[]){
-    printf("Hello World");
-    return 0;
+    // Variables locales
+    char * host = argv[1];
+    char * file = argv[2];
+    char * port = "69";
+
+    struct addrinfo hints;
+    struct addrinfo * result = NULL;
+
+    char frame[BUFFER_SIZE + 4];
+    int s;
+
+    // creatiion de addrinfo
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_INET;      /* Autorise IPv4 */
+    hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+    hints.ai_protocol = IPPROTO_UDP;
+
+    s = getaddrinfo(host, port, &hints, &result);
+        
+
 }
