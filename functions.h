21fd7163412b391c3f3c444441b8a266985472d2
bryanguerineau <bryan.guerineau@outlook.com>
2023-01-18 17:37:28 +0100

    Implementation des fichiers fonctions

---
+++ functions.h
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+
+
+// Variables Globales
+#define BUFFER_SIZE 512
+
